bfe46ca74e6ff641e7ec642625c1bdab01e54b16
nathawat22 <59749844+nathawat22@users.noreply.github.com>
2020-05-07 14:11:59 -0400

    Added area and perimeter code

---
+++ rectArea.py
@@ -0,0 +1,20 @@
+# Program takes user inputs for the length and width of 
+# a rectangle and prints its area and perimeter.
+# =====================================================
+
+# Enter length of the rectangle in feet
+strLength = input("Enter the rectangle length in feet: ")
+strWidth = input("Enter the rectangle width in feet: ")
+
+# Convert length and width to float from string
+rectLength = float(strLength)
+rectWidth = float(strWidth)
+
+# Compute and print area
+area = rectLength * rectWidth
+print("The area of rectangle is ", area, " square feet.")
+
+# Compute and print perimeter
+perimeter = 2* (rectLength + rectWidth)
+print("The perimeter of rectangle is ", perimeter, 
+" feet.")
