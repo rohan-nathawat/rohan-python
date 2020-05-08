587b81420f4fa75cd18934c8acc91cefacfde612
nathawat22 <59749844+nathawat22@users.noreply.github.com>
2020-05-08 12:29:16 -0400

    sum of positive numbers

---
+++ sumNaturalNums.py
@@ -0,0 +1,10 @@
+# Computes and displays sum of given natural numbers
+#===================================================
+
+# Specify a positive number
+NUM = 100
+
+# Compute and print sum
+total = NUM * (NUM + 1) / 2
+print("Sum of first ", NUM, " positive numbers is ",
+total)
